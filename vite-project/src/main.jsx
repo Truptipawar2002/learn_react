4d1d9a1b217cdae4262852e052894df6660d0fda
Trupti pawar <pawartrupti2002@gmail.com>
2025-04-26 16:47:03 +0530

    creat customreact basic project

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,53 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App !</h1>
+        </div> 
+    )
+    
+
+}
+
+// const domElement = document.createElement
+// (reactElement.type)
+// domElement.innerHTML = reactElement.children
+// for (const prop in reactElement.props) {
+//      if (prop === 'children') continue;
+//      domElement.setAttribute(prop, reactElement.props[prop])
+        
+//      }
+//       container.appendChild(domElement)
+        
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target : '_blank'
+
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = ( 
+    <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+const anotherUser = "chai aur react"
+
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com',target : '_blank'},
+    'click me to visit google',
+    anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
-
-    <App />
+     
+    reactElement
 
 )
