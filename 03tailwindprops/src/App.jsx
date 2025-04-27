9e4c3114f7ea3472fe7e41351b63069f3252f891
Trupti pawar <pawartrupti2002@gmail.com>
2025-04-27 23:37:10 +0530

    initial commit

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 class ='bg-green-400 text-black p-4 rounded'>Tailwind Test</h1>
+      
+    </>
+  )
+}
+
+export default App
